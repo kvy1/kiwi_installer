94591a7a892deef66368cbdff719e338040140de
kvy1 <66957919+kvy1@users.noreply.github.com>
2020-10-14 23:49:39 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -2,10 +2,8 @@ $ProgramName = "*Blade & Soul*"
 $link = "https://www.bladeandsoul.com"
 $install = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\*\Products\*\InstallProperties" | Where-Object { $_.getValue('DisplayName') -like $ProgramName } | Where-Object { $_.getValue('HelpLink') -like $link } | ForEach-Object { $_.getValue('InstallLocation') }
 $x = $install
-
 'Courtesy of kiwi :3'
-
-$Urly = "https://raw.githubusercontent.com/kvy1/kiwi/master/git.txt"
+$Urly = "https://raw.githubusercontent.com/kvy1/kiwi/master/public.txt"
 $ZipFiley = 'url.txt'
 $Destination= $x
 Invoke-WebRequest -Uri $Urly -OutFile $x\$ZipFiley
@@ -14,16 +12,11 @@ $ZipFile = 'bnspatch.zip'
 $Destination= $x
 Invoke-WebRequest -Uri $Url -OutFile $x\$ZipFile 
 Expand-Archive -Path $x\$ZipFile -DestinationPath $x -Force
-Expand-Archive -Force -Path $x\Public\bnspatch.zip -DestinationPath $x
-Expand-Archive -Force -Path $x\Public\highpriority.zip -DestinationPath $x
-Expand-Archive -Force -Path $x\Public\lessloadingscreens.zip -DestinationPath $x
-Expand-Archive -Force -Path $x\Public\pluginloader.zip -DestinationPath $x
-Expand-Archive -Force -Path $x\Public\simplemodetrainingroom.zip -DestinationPath $x
-
 $w = [Environment]::GetFolderPath([System.Environment+SpecialFolder]::MyDocuments)
-
-Move-Item -Path $x'\Public\patches.xml' -Destination $w'\bns' -Force
-
+if(!(Test-Path $w'\bns\patches.xml' -PathType leaf))
+{
+Move-Item -Path $x'\patches.xml' -Destination $w'\bns' -Force
+}
 if(!(Test-Path $w'\bns\addons'))
 {
 New-Item -Path $w'\bns' -Name "addons" -ItemType "directory"
@@ -32,12 +25,7 @@ if(!(Test-Path $w'\bns\patches'))
 {
 New-Item -Path $w'\bns' -Name "patches" -ItemType "directory"
 }
-if(!(Test-Path $w'\bns\patches'))
-{
-New-Item -Path $w'\bns' -Name "patches" -ItemType "directory"
-}
+Move-Item -Path $x'\use-ingame-login.xml' -Destination $w'\bns\patches' -Force
 Remove-Item $x\$ZipFile
-Remove-Item $x\Public -recurse -Force
 Remove-Item $x\url.txt -Force
-
-Read-Host "Finished uwu :3"
+Remove-Item $x\patches.xml -Force
